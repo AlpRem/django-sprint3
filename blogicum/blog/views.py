aeb2b377a114c1f01e134ce058b0d55f7b3a792e
Roman <alprem2016@yandex.ru>
2026-06-23 08:17:21 +0300

    change views frim blog

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,22 +1,50 @@
+from django.utils import timezone
+
 from django.shortcuts import render, get_object_or_404
-from blog.models import Post
+from blog.models import Post, Category
 
 
 def index(request):
     template = 'blog/index.html'
-    posts = Post.objects.all()
-    context = {'posts': list(reversed(posts))}
+    posts = (
+        Post.objects.filter(
+            pub_date__lte=timezone.now(),
+            is_published=True,
+            category__is_published=True,
+        )
+        .order_by('-pub_date')[:5]
+    )
+    context = {'post_list': posts}
     return render(request, template, context)
 
 
 def post_detail(request, id):
     template = 'blog/detail.html'
-    post = get_object_or_404(Post, pk=id)
+    post = get_object_or_404(
+        Post,
+        pk=id,
+        is_published=True,
+        pub_date__lte=timezone.now(),
+        category__is_published=True,
+    ).order_by('-pub_date')
     context = {'post': post}
     return render(request, template, context)
 
 
 def category_posts(request, category_slug):
     template = 'blog/category.html'
-    context = {'category_slug': category_slug}
+    category = get_object_or_404(
+        Category,
+        slug=category_slug,
+        is_published=True
+    )
+    post_list = Post.objects.filter(
+        category=category,
+        is_published=True,
+        pub_date__lte=timezone.now()
+    )
+    context = {
+        'category': category,
+        'post_list': post_list,
+    }
     return render(request, template, context)
